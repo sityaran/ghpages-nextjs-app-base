0f1ef906508115d6e89cf2b05da9793cadc1d594
sityaran <sityaran@gmail.com>
2025-03-03 00:02:13 -0800

    fixing base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "ghpages-nextjs-app-base",
+  basePath: "/ghpages-nextjs-app-base",
   images: {
     unoptimized: true,
   },
